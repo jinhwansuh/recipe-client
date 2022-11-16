a71b230bf1b42d7ae0e7121d72aef8294ba4340c
jinhwansuh <nick02190219@gmail.com>
2022-11-16 18:07:00 +0900

    feat: chakra ui와 react hook form 연결

--- pages/upload/index.tsx
+++ pages/upload/index.tsx
@@ -1,65 +1,92 @@
 import { useMutation } from '@apollo/client';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { ChangeEvent, useState } from 'react';
+import { useForm, useFieldArray } from 'react-hook-form';
+import { IngredientsInput } from '~/components';
 import { CREATE_RECIPE } from '~/graphql/Mutations';
 import { RecipeAttributes } from '~/types/recipe';
 
+type Ingredient = {
+  name: string;
+  weigh: string;
+  selected: string;
+};
+
+export interface Inputs {
+  title: string;
+  uploader: string;
+  videoURL: string;
+  order: string;
+  ingredient: Ingredient[];
+}
+
 const UploadPage = () => {
-  const [input, setInput] = useState<RecipeAttributes>({
-    title: '',
-    videoURL: '',
-    order: '',
-    ingredients: '',
-    uploader: '',
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      ingredient: [{ name: '', weigh: '', selected: '' }],
+    },
+  });
+  const { fields, append } = useFieldArray({
+    control,
+    name: `ingredient`,
   });
 
   const [createRecipe, { error }] = useMutation(CREATE_RECIPE);
-  const handleButtonClick = () => {
-    console.log(input);
-    const { title, videoURL, order, ingredients, uploader } = input;
-    createRecipe({
-      variables: { title, videoURL, order, ingredients, uploader },
-    });
+  // const handleButtonClick = () => {
+  //   console.log(input);
+  //   const { title, videoURL, order, ingredients, uploader, measure } = input;
+  //   createRecipe({
+  //     variables: { title, videoURL, order, ingredients, uploader, measure },
+  //   });
+  // };
+
+  const onSubmit = (data: Inputs) => {
+    console.log(data);
+    console.log(123);
   };
 
-  const handleInputChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setInput((prev) => ({ ...prev, [name]: value }));
+  const handleAddButton = () => {
+    append({ name: '', weigh: '', selected: '' });
   };
 
   return (
-    <div>
-      <div>
-        <label>title</label>
-        <input name='title' onChange={handleInputChange}></input>
-      </div>
-      <div>
-        <label>videoURL</label>
-        <input name='videoURL' onChange={handleInputChange}></input>
-      </div>
-      <div>
-        <label>order</label>
-        <textarea name='order' onChange={handleInputChange}></textarea>
-      </div>
-      <div>
-        <label>ingredients</label>
-        <input name='ingredients' onChange={handleInputChange}></input>
-      </div>
-      <div>
-        <label>uploader</label>
-        <input name='uploader' onChange={handleInputChange}></input>
-      </div>
-      <button
-        onClick={() => {
-          handleButtonClick();
-        }}
-      >
-        업로드
-      </button>
-    </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isRequired>
+          <FormLabel>제목</FormLabel>
+          <Input {...register('title', { required: true })} />
+          <FormLabel>유튜버</FormLabel>
+          <Input {...register('uploader', { required: true })} />
+          <FormLabel>videoURL</FormLabel>
+          <StyledInput {...register('videoURL', { required: true })} />
+          <FormLabel>순서</FormLabel>
+          <Textarea {...register('order', { required: true })} />
+        </FormControl>
+
+        <Button onClick={handleAddButton}>+</Button>
+        <div>
+          <Button type='submit'>Upload</Button>
+        </div>
+      </form>
+    </>
   );
 };
+
+const StyledInput = styled(Input)`
+  width: 80%;
+`;
 
 export default UploadPage;
