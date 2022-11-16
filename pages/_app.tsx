0e8da1be1e275b1f9fb7773996caaf450bb62c62
jinhwansuh <nick02190219@gmail.com>
2022-11-16 15:47:36 +0900

    feat: chakra ui 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
+import { ApolloProvider } from '@apollo/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ApolloProvider } from '@apollo/client';
 import client from '~/lib/apollo';
+import theme from './styles/ChakraProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ApolloProvider>
   );
 }
