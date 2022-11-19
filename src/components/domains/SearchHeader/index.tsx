79b1f57a2afa949e745cc93f218d913d3c82f321
jinhwansuh <nick02190219@gmail.com>
2022-11-19 17:28:31 +0900

    feat: query를 이용한 router 구현

--- src/components/domains/SearchHeader/index.tsx
+++ src/components/domains/SearchHeader/index.tsx
@@ -11,15 +11,17 @@ import {
 } from '@chakra-ui/react';
 
 const SearchHeader = () => {
+  const router = useRouter();
   const [inputState, setInputState] = useState({
     select: 'tag',
     search: '',
   });
-  const router = useRouter();
 
   const handleSearchClick = () => {
-    console.log(inputState);
-    // router.push(`asdf`);
+    router.push({
+      pathname: '/search',
+      query: { [inputState.select]: inputState.search },
+    });
   };
   return (
     <header>
@@ -37,7 +39,7 @@ const SearchHeader = () => {
           >
             <option value={'title'}>제목</option>
             <option value={'tag'}>요리</option>
-            <option value={'ingredient'}>재료</option>
+            <option value={'ingredients'}>재료</option>
             <option value={'uploader'}>유튜버</option>
           </Select>
 
