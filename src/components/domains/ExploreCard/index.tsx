0c947b5f1b6b507e4339dcf30847b82baaf89077
jinhwansuh <nick02190219@gmail.com>
2022-11-26 17:03:19 +0900

    design: chakra ui로 디자인 되어 있던 것 styled를 사용해 변경

--- src/components/domains/ExploreCard/index.tsx
+++ src/components/domains/ExploreCard/index.tsx
@@ -1,17 +1,8 @@
 import Link from 'next/link';
-import {
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  Heading,
-  HStack,
-  Image,
-  Stack,
-  Text,
-} from '@chakra-ui/react';
+import { Button, Heading, Image, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { RecipeAttributes } from '~/types/recipe';
-import { getYoutubeThumbnail } from '~/utils/convert';
+import { getIngredientArray, getYoutubeThumbnail } from '~/utils/convert';
 
 interface Props {
   id: string;
@@ -19,51 +10,74 @@ interface Props {
 }
 
 const ExploreCard = ({ id, data }: Props) => {
+  const { videoURL, title, ingredients, tag } = data;
+  const ingredientArray = getIngredientArray(ingredients);
+
   return (
     <Link href={`/detail/${id}`}>
-      <Card
-        direction={{ base: 'column', sm: 'row' }}
-        overflow='hidden'
-        variant='outline'
-        w={800}
-        _hover={{
-          background: 'white',
-          color: 'purple',
-        }}
-      >
-        <Image
-          objectFit='cover'
-          maxW={{ base: '100%', sm: '200px' }}
-          src={getYoutubeThumbnail(data.videoURL)}
-          alt='thumbnail'
-          _hover={{
-            cursor: 'pointer',
-          }}
-        />
-
-        <Stack>
-          <CardBody
-            _hover={{
-              cursor: 'pointer',
-            }}
-          >
-            <Heading size='md'>{data.title}</Heading>
-            <HStack>
-              <Text w={'90px'}>재료</Text>
-              <Text py='2'>{data.ingredients}</Text>
-            </HStack>
-          </CardBody>
-
-          <CardFooter>
+      <StyledCardContainer>
+        <StyledImageWrapper>
+          <Image
+            objectFit='fill'
+            w={'100%'}
+            // maxW={{ base: '100%', sm: '200px' }}
+            src={getYoutubeThumbnail(videoURL)}
+            alt='thumbnail'
+          />
+        </StyledImageWrapper>
+        <StyledDetailWrapper>
+          <Heading size={'md'} wordBreak={'keep-all'}>
+            {title}
+          </Heading>
+          <StyledIngredientWrapper>
+            <Text w={'40px'}>재료 :</Text>
+            <StyledIngredientDetails>
+              {ingredientArray.map((ingredient, index) => (
+                <Text as={'span'} key={index} px='1'>
+                  {ingredient[0]}
+                </Text>
+              ))}
+            </StyledIngredientDetails>
+          </StyledIngredientWrapper>
+          <StyledFoodWrapper>
+            <Text>요리 : {tag}</Text>
             <Button variant='solid' colorScheme='blue'>
               Buy Latte
             </Button>
-            tag: {data.tag}
-          </CardFooter>
-        </Stack>
-      </Card>
+          </StyledFoodWrapper>
+        </StyledDetailWrapper>
+      </StyledCardContainer>
     </Link>
   );
 };
+
+const StyledCardContainer = styled.div`
+  display: flex;
+  width: 800px;
+  margin: 5px 0;
+  box-sizing: border-box;
+`;
+
+const StyledImageWrapper = styled.div`
+  width: 300px;
+`;
+
+const StyledDetailWrapper = styled.div`
+  flex: 1;
+  padding: 3px 5px;
+`;
+
+const StyledIngredientWrapper = styled.div`
+  display: flex;
+`;
+
+const StyledIngredientDetails = styled.div`
+  flex: 1;
+`;
+
+const StyledFoodWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export default ExploreCard;
